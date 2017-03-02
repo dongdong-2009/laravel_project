6b8cc7a63768aa210c3954e517fb2edfd82f55c0
小昆哥 <969352531@qq.com>
2017-03-02 11:23:55 +0800

    larvael测试配置，路由位于route/web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,9 +18,23 @@ Route::get('/', function () {
 //获取环境变量测试
 Route::get('/getenv',function() {
 	$app_url = env('APP_URL','我是默认值,.env文件没有设置我');
-	if($app_url){
-		return ['status'=>'OK','app_url'=>$app_url];
+	$environment = App::environment();
+	if($app_url && $environment){
+		return ['status'=>'OK',
+		'app_url'=>$app_url,
+		'environment'=>$environment];
 	}else{
 		return ['status'=>'error'];
+	}
+});
+
+//访问配置值测试
+Route::get('/getconfig',function() {
+	//config(['database.connections'=>"我已经设置了"]);
+	$value = config('database.connections');
+	if(!$value){
+		return ['status'=>'error'];
+	}else{
+		return ['status'=>'OK','databases.connections'=>$value];
 	}
 });
